7d31981bd17ad303a9cf96a084845dcedecf48d2
MinhNhatA3 <88643204+Rainsh0wer@users.noreply.github.com>
2023-05-23 16:42:27 +0700

    Update hack.js

--- resources/hack.js
+++ resources/hack.js
@@ -19,22 +19,22 @@ function SimpleLiveworksheetHack(jsonlang) {
             } else if(arr[incr][0].includes('tick:')) {
                 element = document.querySelectorAll('[id^="tickbox"]')[tick];
                 arr[incr][0].includes('yes')?element.click():{};
-                tick++; console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: white; font-size: 12px') 
+                tick++; console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: black; font-size: 12px') 
             } else if(arr[incr][0].includes('drag:')) {
                 element = Array.from(document.querySelectorAll('[id^="dragablediv"]')).filter(b => (b.style.top.includes(arr[incr][1]) && b.style.left.includes(arr[incr][2])))[0];
                 drag = contenidorellenado[contenidorellenado.findIndex(entry => entry[0].includes(arr[incr][0].replace('drag:','drop:')))];
                 element.style.top = drag[1]+"px"; element.style.left = drag[2]+"px";
-                console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: white; font-size: 12px')
+                console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: black; font-size: 12px')
             } else if(arr[incr][0].includes('choose:')) {
                 element = document.querySelectorAll('[id^="selectbox"]')[choose];
                 ans = arr[incr][0].replace('choose:','').split('/').findIndex(entry => entry.includes("*")), element.selectedIndex = ans+1;
-                choose++; console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: white; font-size: 12px') 
+                choose++; console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: black; font-size: 12px') 
             } else if (arr[incr][0].includes('join:')) {
                 drag = contenidorellenado.filter(entry => (entry[0] == arr[incr][0]))[1]; if(drag) {
                 let yvmShadow = document.createElementNS('http://www.w3.org/2000/svg', "line"); 
                 yvmShadow.setAttribute("x1",drag[2]+drag[4]/2); yvmShadow.setAttribute("y1",drag[1]+random(drag[3])); yvmShadow.setAttribute("x2",arr[incr][2]+arr[incr][4]/2); yvmShadow.setAttribute("y2",arr[incr][1]+random(arr[incr][3])); yvmShadow.setAttribute("stroke","darkblue"); yvmShadow.style.stroke = "darkblue"; yvmShadow.style.strokeWidth = "5px"
                 document.querySelector('#elsvgdefinitivo').appendChild(yvmShadow); drag[5] = arr[incr][0]; drag[0].replace('join', 'joined'); arr[incr][5] = arr[incr][0]; }; arr[incr][0] = arr[incr][0].replace('join', 'joined'); 
-                console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: white; font-size: 12px') 
+                console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #ffffff; color: black; font-size: 12px') 
             } else if (arr[incr][0].includes('listen') && arr[incr][0].includes(':') || arr[incr][0].includes('joined:') || arr[incr][0].includes('drop:') || arr[incr][0].includes('https://youtu.be/')) {
             } else if (arr[incr][0] =='') {
                 text++; console.log("%c"+incr+"."+json.free, 'background: yellow; color: black; font-size: 12px'); 
@@ -43,12 +43,12 @@ function SimpleLiveworksheetHack(jsonlang) {
                 element = document.querySelectorAll('[id^="textbox"]')[text];
                 element.textContent = arr[incr][0];
                 text++; console.log("%c"+incr+"."+json.success+" => "+arr[incr][0], 'background: #cfe2f3; color: black; font-size: 12px') 
-            } else console.log("%c"+incr+"."+json.error+" => "+arr[incr][0], 'background: #5e0000; color: white; font-size: 12px') 
+            } else console.log("%c"+incr+"."+json.error+" => "+arr[incr][0], 'background: #5e0000; color: black; font-size: 12px') 
             incr++;
             CheckAnswers(arr, delay);
             }, delay)
         } else {
-            console.log("%c"+json.completed+(nulos>0?" "+json.none.replace('<number>', nulos):""), 'background: #005e06; color: white; font-size: 16px');
+            console.log("%c"+json.completed+(nulos>0?" "+json.none.replace('<number>', nulos):""), 'background: #005e06; color: black; font-size: 16px');
             console.log('%c'+json.dev, 'color: #804949');
             document.querySelector("#capafinish").ariaChecked = true;
         }
